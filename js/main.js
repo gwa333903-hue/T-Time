a5d8493b602f8022deb0a513edb48bc38aab897b
gwa333903-hue <gwa333903@gmaik.com>
2025-11-30 22:44:04 +0530

    Re

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-let lastScrollTop = 0;
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        // Downscroll
-        header.classList.add('hidden');
-    } else {
-        // Upscroll
-        header.classList.remove('hidden');
-    }
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-});
+/* Reverted */
